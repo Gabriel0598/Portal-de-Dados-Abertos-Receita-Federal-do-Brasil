e60f0aa64c8bfebd91cfdf015635f941c83f23c0
Gabriel Augusto Ferreira <gabrielaugustoferreira@outlook.com>
2025-01-28 03:11:40 -0300

    Montagem de função para leitura e extração a partir de endpoint

--- src/pipelines/bronze/di-dados_abertos_cnpj.py
+++ src/pipelines/bronze/di-dados_abertos_cnpj.py
@@ -1,2 +1,42 @@
-import requests
+import os, requests, zipfile
+from io import BytesIO
 
+def download_and_extract_data(end_url, destination_folder):
+    """
+    Realiza download de arquivo zip a partir de endpoint e extrai conteúdo para diretório interno
+    """
+    try:
+        # Verifica existencia de diretório
+        if not os.path.exists(destination_folder):
+            os.makedirs(destination_folder)
+
+        # Realiza download de arquivo zip
+        print(f"Download de arquivo a partir de: {end_url}")
+        response = requests.get(end_url, stream=True)
+        # Em caso de falha é invocada exceção
+        response.raise_for_status()
+
+        # Leitura do zip a partir da memória
+        with zipfile.ZipFile(BytesIO(response.content)) as zfile:
+            # Lista arquivos do zip
+            file_list = zfile.namelist()
+            print(f"Arquivos encontrados: {file_list}")
+
+            print(f"Extraindo arquivos para {destination_folder}")
+            zfile.extractall(destination_folder)
+            print("Extração com sucesso")
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao realizar download: {e}")
+    except zipfile.BadZipFile:
+        print("Arquivo baixado não corresponde a zip")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+
+# Endpoint origem
+end_url = "https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/?C=N;O=D"
+
+# Diretório destino
+destination_folder = "/data/bronze/land_zone"
+
+# Invocação da função de busca e extração do endpoint
+download_and_extract_data(end_url, destination_folder)
