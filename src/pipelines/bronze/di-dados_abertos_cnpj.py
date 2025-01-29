dcc468a1de6d1eb3100e90f1c71a949f54876757
Gabriel Augusto Ferreira <gabrielaugustoferreira@outlook.com>
2025-01-29 02:38:39 -0300

    Tratamento condicional para requisição de arquivos/ Definição de paths roots e extract/ Dupla chamada de urls para extração

--- src/pipelines/bronze/di-dados_abertos_cnpj.py
+++ src/pipelines/bronze/di-dados_abertos_cnpj.py
@@ -16,32 +16,58 @@ def download_and_extract_data(end_url, destination_folder):
         # Em caso de falha é invocada exceção
         response.raise_for_status()
 
-        # Leitura do zip a partir da memória
-        with zipfile.ZipFile(BytesIO(response.content)) as zfile:
-            # Lista arquivos do zip
-            file_list = zfile.namelist()
-            print(f"Arquivos encontrados: {file_list}")
+        # Verifica se download foi bem sucedido e extrai os dados
+        if response.status_code == 200:
+            # Leitura do zip a partir da memória
+            with zipfile.ZipFile(BytesIO(response.content)) as zfile:
+                # Lista arquivos do zip
+                file_list = zfile.namelist()
+                print(f"Arquivos encontrados: {file_list}")
 
-            print(f"Extraindo arquivos para {destination_folder}")
-            zfile.extractall(destination_folder)
-            print("Extração com sucesso")
+                print(f"Extraindo arquivos para {destination_folder}")
+                zfile.extractall(destination_folder)
+                print("Extração com sucesso")
+        elif response.status_code == 400:
+            print("Requisição inválida")
+        elif response.status_code == 401:
+            print("Bad request")
+        elif response.status_code == 403:
+            print("Acesso negado")
+        elif response.status_code == 404:
+            print("Arquivo não encontrado no endpoint")
+        else:
+            print(f"Erro inesperado: HTTP {response.status_code}")
     except requests.exceptions.RequestException as e:
         print(f"Erro ao realizar download: {e}")
     except zipfile.BadZipFile:
-        print("Arquivo baixado não corresponde a zip")
+        print("Arquivo consultado não corresponde a zip")
     except Exception as e:
         print(f"Erro inesperado: {e}")
 
+# Path relativo do projeto
+project_root = os.path.dirname(os.path.abspath(__file__))
+destination_folder = os.path.join(project_root, "/data/bronze/land_zone")
+
+# ----------------------------------------------------------------
 # Endpoint origem - Arquivos empresas
 year_search="2025"
 month_search="01"
-file_zip = "Empresas1.zip"
+file1_zip = "Empresas1.zip"
 
-# URL para busca de arquivos especificos
-end_url = f"https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/{year_search}-{month_search}/{file_zip}"
+# URL para busca de arquivos RFB
+end_url = f"https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/{year_search}-{month_search}/{file1_zip}"
 
-# Diretório destino
-destination_folder = "/data/bronze/land_zone"
+# Invocação da função de busca e extração do endpoint
+download_and_extract_data(end_url, destination_folder)
+
+# ----------------------------------------------------------------
+# Endpoint origem - Arquivos sócios
+year_search = "2025"
+month_search = "01"
+file2_zip = "Socios1.zip"
+
+# URL para busca de arquivos RFB
+end_url = f"https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/{year_search}-{month_search}/{file2_zip}"
 
 # Invocação da função de busca e extração do endpoint
 download_and_extract_data(end_url, destination_folder)
