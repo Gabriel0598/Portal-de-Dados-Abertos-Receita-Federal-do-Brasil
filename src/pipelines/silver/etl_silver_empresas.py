22283e19ee8c01b7854b49b0389040eae1662e03
Gabriel Augusto Ferreira <gabrielaugustoferreira@outlook.com>
2025-02-05 04:09:56 -0300

    Comentarios no arquivo silver

--- src/pipelines/silver/etl_silver_empresas.py
+++ src/pipelines/silver/etl_silver_empresas.py
@@ -1,12 +1,16 @@
 import os
 from pyspark.sql import SparkSession
 
+# Unset SPARK_HOME environment variable
+# if 'SPARK_HOME' in os.environ:
+#    del os.environ['SPARK_HOME']
+
 spark = (SparkSession.builder
              .appName("etl_silver_socios_empresas")
               .config("spark.databricks.service.server.enabled", "true")
                 .getOrCreate())
 
-# bronze_path_empre = os.path.join(os.getcwd(), 'data/bronze/land_zone/*EMPRECSV')
+bronze_path_empre = os.path.join(os.getcwd(), 'data/bronze/land_zone/*EMPRECSV')
 
 # df = spark.read.csv(bronze_path_empre, header=True, inferSchema=True)
 # df.printSchema()
